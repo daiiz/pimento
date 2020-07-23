15ae4483852b1cd5c90138ea3f648790a01517a4
daiiz <da.iz.mg@gmail.com>
2020-07-24 01:42:58 +0900

    onmessageの練習

--- client/index.js
+++ client/index.js
@@ -5,3 +5,18 @@ window.addEventListener('load', async event => {
   await convertImages({ gyazoIds: ['dc220ec3da67354f35d5a30aafcdf2f6', 'ddb8f03e5a404862c866d2b95563cd67'] })
   await convertTexDocument()
 }, false)
+
+window.onmessage = function ({ origin, data }) {
+  if (origin !== 'https://scrapbox.io') return
+  const { task, type, body } = data
+  switch (task) {
+    case 'transfer-data': {
+      console.log('>>>>!', type, body)
+      break
+    }
+    case 'close': {
+      this.close()
+      break
+    }
+  }
+}
