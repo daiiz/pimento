9d75650dd877c05b352e97d7bcaa32f2f1499ae0
daiiz <da.iz.mg@gmail.com>
2020-08-15 22:40:37 +0900

    参照をテスト

--- client/scrapboxlib/test/lstlisting.js
+++ client/scrapboxlib/test/lstlisting.js
@@ -3,7 +3,7 @@ const { runTest } = require('./helper')
 
 const cases = [
   {
-    name: 'indent level 0',
+    name: 'indent level 0 codeblock',
     source: [
       'code:hello.js',
       '\tconsole.log("Hello,");',
@@ -13,6 +13,22 @@ const cases = [
       '\\begin{lstlisting}[frame=tb,caption=hello.js]',
       'console.log("Hello,");',
       'console.log("World!");',
+      '\\end{lstlisting}'
+    ]
+  },
+  {
+    name: 'indent level 0 codeblock with label',
+    source: [
+      'Look at [. code:my-svg].',
+      'code:ref=my-svg, image.svg',
+      '\t<svg xmlns="http://www.w3.org/2000/svg">',
+      '\t</svg>'
+    ],
+    expect: [
+      'Look at \\autoref{code:my-svg}.',
+      '\\begin{lstlisting}[frame=tb,label=code:my-svg,caption=image.svg]',
+      '<svg xmlns="http://www.w3.org/2000/svg">',
+      '</svg>',
       '\\end{lstlisting}'
     ]
   },
