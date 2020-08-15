8055633d0dc1275d2f6508b7d2261962a71cfdb0
daiiz <da.iz.mg@gmail.com>
2020-08-15 22:27:40 +0900

    add tests

--- client/scrapboxlib/test/lstlisting.js
+++ client/scrapboxlib/test/lstlisting.js
@@ -2,7 +2,58 @@
 const { runTest } = require('./helper')
 
 const cases = [
-
+  {
+    name: 'indent level 0',
+    source: [
+      'code:hello.js',
+      '\tconsole.log("Hello,");',
+      '\tconsole.log("World!");'
+    ],
+    expect: [
+      '\\begin{lstlisting}[frame=tb,caption=hello.js]',
+      'console.log("Hello,");',
+      'console.log("World!");',
+      '\\end{lstlisting}'
+    ]
+  },
+  {
+    name: 'user-tex block',
+    source: [
+      'code:tex',
+      '\t% Comments',
+      '\t\\begin{displaymath}',
+      '\t  \\int^{b}_{a} f(x) dx = \\lim_{n \\to \\infty} \\sum^{n-1}_{i=0} f(x_{i}) \\Delta x',
+      '\t\\end{displaymath}'
+    ],
+    expect: [
+      '%===== <user-tex> =====',
+      '% Comments',
+      '\\begin{displaymath}',
+      '  \\int^{b}_{a} f(x) dx = \\lim_{n \\to \\infty} \\sum^{n-1}_{i=0} f(x_{i}) \\Delta x',
+      '\\end{displaymath}',
+      '%===== </user-tex> ====='
+    ]
+  },
+  {
+    name: 'keep blank lines',
+    source: [
+      'code:blank.html',
+      '\t<blank>',
+      '\t', '\t', '\t',
+      '\t<!-- Hello -->',
+      '\t', '\t',
+      '\t</blank>'
+    ],
+    expect: [
+      '\\begin{lstlisting}[frame=tb,caption=blank.html]',
+      '<blank>',
+      '', '', '',
+      '<!-- Hello -->',
+      '', '',
+      '</blank>',
+      '\\end{lstlisting}'
+    ]
+  }
 ]
 
 runTest('Convert to lstlisting', cases)
