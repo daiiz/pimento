613c6b30af1bbdfa8070ed115b3af2868beeab72
daiiz <da.iz.mg@gmail.com>
2020-08-20 04:44:15 +0900

    外部URLを脚注表記する際にescape忘れ

--- client/scrapboxlib/texify.js
+++ client/scrapboxlib/texify.js
@@ -88,13 +88,13 @@ const Texify = node => {
         }
       } else if (pathType === 'absolute') {
         if (node.content) {
-          return `${texEscape(node.content)}${backSlash}footnote{${backSlash}url{` + href + '}}'
+          return `${texEscape(node.content)}${backSlash}footnote{${backSlash}url{` + texEscape(href) + '}}'
         } else {
           return `${backSlash}url{` + texEscape(href) + '}'
         }
       } else if (pathType === 'root') {
         // 外部プロジェクトへの参照
-        const url = 'https://scrapbox.io' + href
+        const url = texEscape('https://scrapbox.io' + href)
         return `${backSlash}url{` + texEscape(href) + '}' + `${backSlash}footnote{${backSlash}url{` + url + '}}'
       }
       break
