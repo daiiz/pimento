522f36fb17d895549a533ae640c81654fd5087b1
Daiki Iizuka <da.iz.mg@gmail.com>
2020-08-26 02:47:24 +0900

    深さ5以上の箇条書きをつくらない (#26)
    * 箇条書きの深さを5以上にしない
    
    * add tests

--- client/scrapboxlib/test/itemize.js
+++ client/scrapboxlib/test/itemize.js
@@ -48,6 +48,50 @@ const cases = [
     ]
   },
   {
+    // https://gyazo.com/b1e090896cbab98a6769aeadd4fad6ea
+    name: 'do not create bullets with a depth of 5 or more',
+    source: [
+      'Indent level (itemizeIndentStack size)',
+      '\t1 (0)',
+      '\t\t2 (1)',
+      '\t\t\t3 (2)',
+      '\t\t\t\t4 (3)',
+      '\t\t\t\t4',
+      '\t\t\t3 again',
+      '\t\t\t\t4 (3)',
+      '\t\t\t\t\t5 (4)',
+      '\t\t\t\t\t5',
+      '\t\t\t\t\t\t6 (5)',
+      '\t\t\t\t4 again',
+      '\t\t\t\t\t5 again'
+    ],
+    expect: [
+      'Indent level (itemizeIndentStack size)',
+      '\\begin{itemize}', // Open 1
+      '  \\item 1 (0)',
+      '  \\begin{itemize}', // Open 2
+      '    \\item 2 (1)',
+      '    \\begin{itemize}', // Open 3
+      '      \\item 3 (2)',
+      '      \\begin{itemize}', // Open 4
+      '        \\item 4 (3)',
+      '        \\item 4',
+      '      \\end{itemize}', // Close 4
+      '      \\item 3 again',
+      '      \\begin{itemize}', // Open 4
+      '        \\item 4 (3)',
+      '          \\item 5 (4)',
+      '          \\item 5',
+      '            \\item 6 (5)',
+      '        \\item 4 again',
+      '          \\item 5 again',
+      '      \\end{itemize}', // Close4
+      '    \\end{itemize}', // Close 3
+      '  \\end{itemize}', // Close 2
+      '\\end{itemize}' // Close 1
+    ]
+  },
+  {
     name: 'indent with URL',
     source: ['\thttps://example.com/'],
     expect: [
