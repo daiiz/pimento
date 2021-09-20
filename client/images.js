d7d591c9ad74d829b5cd28b3b8d16aa1faa12458
daiiz <da.iz.mg@gmail.com>
2021-09-20 22:16:03 +0900

    画像アップロード処理を分離

--- client/images.js
+++ client/images.js
@@ -1,23 +1,26 @@
 /* eslint-env browser */
 const { getGyazoImageId } = require('./scrapboxlib/lib')
 
-const headers = {
-  'Content-Type': 'application/json; charset=utf-8'
-}
-
-const uploadImages = async ({ gyazoIds }) => {
-  if (!gyazoIds || gyazoIds.length === 0) return {}
+const uploadImages = async gyazoIds => {
+  if (!gyazoIds || gyazoIds.length === 0) {
+    return {}
+  }
   const res = await fetch('/api/convert/images', {
     method: 'POST',
-    headers,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
     body: JSON.stringify({ gyazoIds })
   })
-  if (!res.ok) throw new Error('Response is not ok')
+  if (!res.ok) {
+    throw new Error('Response is not ok')
+  }
   const data = await res.json()
   console.log('imageData:', data)
+  return data
 }
 
-const uploadGyazoIcons = async pageGyazoIconUrls => {
+const extractGyazoIcons = async pageGyazoIconUrls => {
   const dict = Object.create(null)
   for (const titleLc of Object.keys(pageGyazoIconUrls)) {
     const gyazoId = getGyazoImageId(pageGyazoIconUrls[titleLc])
@@ -25,11 +28,11 @@ const uploadGyazoIcons = async pageGyazoIconUrls => {
       dict[titleLc] = gyazoId
     }
   }
-  await uploadImages({ gyazoIds: Object.values(dict) })
+  // await uploadImages({ gyazoIds: Object.values(dict) })
   return dict
 }
 
 module.exports = {
   uploadImages,
-  uploadGyazoIcons
+  extractGyazoIcons
 }
