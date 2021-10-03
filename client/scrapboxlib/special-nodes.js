74060a0844ba511e3a6b3004827827f28b765961
daiiz <da.iz.mg@gmail.com>
2021-10-03 18:53:54 +0900

    note

--- client/scrapboxlib/special-nodes.js
+++ client/scrapboxlib/special-nodes.js
@@ -31,7 +31,8 @@ const handleSpecialLine = (line, title) => {
         // https://scrapbox.io/teamj/pimento_v2:_節の埋め込み記法
         const hash = addToPageRefs(line._text)
         incrementPageEmbedCounter(hash)
-        console.log("###", title, "->", hash)
+        // TODO: 参照グラフを更新 (描画実績は見ないので表示されていることは保証しない)
+        console.log("###", `${title} (${calcPageTitleHash(title)})`, "->", hash)
         return [`\$\{window.funcs.page_${hash}(level + 1 + ${line._level})\}`]
       } else {
         return [`\$\{window.textBlockName(level + 1 + ${line._level}, showNumber)\}${line._text}}`]
